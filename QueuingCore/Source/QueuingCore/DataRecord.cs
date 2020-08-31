b9c478231c57459a64f5f8ab0babea78912d9418
Matthes <mk.public@outlook.com>
2020-08-31 08:56:03 +0200

    IID Interface and DataRecord

---
+++ QueuingCore/Source/QueuingCore/DataRecord.cs
@@ -0,0 +1,34 @@
+﻿namespace QueuingCore
+{
+    public class DataRecord
+    {
+        public int Id { get; set; }
+
+        public string CustomerClass { get; set; }
+
+        //todo: int type correct?
+        public int NodeId { get; set; }
+
+        public double ArrivalDate { get; set; }
+
+        public double WaitingTime { get; set; }
+        
+        public double ServiceStartDate { get; set; }
+
+        public double ServiceTime { get; set; }
+
+        public double ServiceEndDate { get; set; }
+
+        public double BlockedTime { get; set; }
+
+        public double ExitDate { get; set; }
+
+        //todo: string type correct?!
+        public string Destination { get; set; }
+
+        public int QueueSizeAtArrival { get; set; }
+
+        public int QueueSizeAtDeparture { get; set; }
+
+    }
+}
